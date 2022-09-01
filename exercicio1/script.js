975776d3ba35c1389a022a442c934178d8808c83
mayaracgc <mayaracgcgmail.com>
2022-09-01 20:34:53 -0300

    exercicio feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+// O primeiro deve possuir apenas números, 
+//e estes números não devem seguir nenhuma ordem específica;
+const array1 = [22 ,88 , 66 , 44]
+// O segundo array deve possuir apenas strings;
+const array2 = ["livro" , "café" , "chuva"]  
+// O terceiro array deve possuir números, strings e booleanos. Imprima cada um dos arrays acima, utilizando
+const array3 = [22 , "livro" , 88 > 66]
+
+console.log(array1 , array2, array3) 
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const array1 = [22 ,88 , 66 , 44]
+const array2 = ["livro" , "café" , "chuva"] 
+const array3 = [22 , "livro" , 88 > 66]
+console.log(array1 , array2, array3) 
+// Imprima a quantidade de itens de cada array (utilize um console.log() para cada impressão);
+console.log(array1.length , array2.length , array3.length) 
+// Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array;
+console.log(array1[0])
+console.log(array2[1])
+console.log(array3[2])
+// Imprima uma informação booleana verdadeira sobre a inclusão de um item do primeiro array, e uma informação booleana falsa sobre a inclusão de um item ao terceiro array.
+// Isto é, um includes() verdadeiro, e outro falso.
+console.log(array1.includes(88 == 88))
+console.log(array2.includes(22 === "livro"))
